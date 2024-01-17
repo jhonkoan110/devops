260fc8e0719efdd6c5d793dd1312d7b9e1050cee
Кирилл Капитальцев <kkapitaltsev@alfabank.ru>
2024-01-17 16:34:08 +0300

    add ts lint

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,8 @@
 module.exports = {
-  ...require('gts/.prettierrc.json')
-}
+  ...require('gts/.prettierrc.json'),
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 4,
+  trailingComma: 'all',
+};
